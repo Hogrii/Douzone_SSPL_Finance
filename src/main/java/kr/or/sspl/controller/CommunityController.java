9ad8f8de45e0925d735c8c57f64f4f45df30876d
LJH <hosboy93@naver.com>
2023-06-30 09:25:30 +0900

    ✨ : css 수정

--- src/main/java/kr/or/sspl/controller/CommunityController.java
+++ src/main/java/kr/or/sspl/controller/CommunityController.java
@@ -2,28 +2,30 @@ package kr.or.sspl.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("Community")
 public class CommunityController {	
-	@GetMapping("list.do")
+	@GetMapping("/list.do")
 	public String list() {
 		System.out.println("진입1");
 		return "community/community_list";
 	}
 	
-	@GetMapping("detail.do")
+	@GetMapping("/detail.do")
 	public String detail() {
 		System.out.println("진입2");
 		return "community/community_detail";
 	}
 	
-	@GetMapping("modify.do")
+	@GetMapping("/modify.do")
 	public String modify() {
 		System.out.println("진입3");
 		return "community/community_modify";
 	}
 	
-	@GetMapping("write.do")
+	@GetMapping("/write.do")
 	public String write() {
 		System.out.println("진입4");
 		return "community/community_write";
